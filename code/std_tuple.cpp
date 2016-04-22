9b1a65b5e08b0b4578f3d539c25078e15ebf3cfb
Louis Dionne <ldionne.2@gmail.com>
2016-04-22 17:28:13 -0400

    Put std::tuple examples into a sample file

---
+++ code/std_tuple.cpp
@@ -0,0 +1,39 @@
+// Copyright Louis Dionne 2016
+// Distributed under the Boost Software License, Version 1.0.
+
+#include <cmath>
+#include <tuple>
+
+
+// sample(spherical)
+std::tuple<double, double, double>
+from_spherical(double r, double theta, double phi) {
+    double x = r * std::sin(theta) * std::cos(phi);
+    double y = r * std::sin(phi) * std::sin(phi);
+    double z = r * std::cos(theta);
+    return {x, y, z};
+}
+
+std::tuple<double, double, double> xyz = from_spherical(3, 16.5, 25.5);
+double x = std::get<0>(xyz),
+       y = std::get<1>(xyz),
+       z = std::get<2>(xyz);
+// end-sample
+
+// sample(animals)
+struct Fish { };
+struct Cat { };
+struct Dog { };
+
+std::tuple<Fish, Cat, Dog> my_animals() {
+    // ...
+    return {};
+}
+
+std::tuple<Fish, Cat, Dog> animals = my_animals();
+Fish fish = std::get<0>(animals);
+Cat  cat  = std::get<1>(animals);
+Dog  dog  = std::get<2>(animals);
+// end-sample
+
+int main() { }
